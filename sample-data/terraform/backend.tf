689d856b59fa7ba982e0fdfe308d4f4d1767c3f8
aleindecker@google.com <aleindecker@google.com>
2025-05-07 19:41:20 +0000

    updating config and file settings

--- sample-data/terraform/backend.tf
+++ sample-data/terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
-    bucket = "<TERRAFORM_BUCKET>"
-    prefix = "<TERRAFORM_ENV>"
+    bucket = "aef-annie-l-advisory-project-tfe"
+    prefix = "sample-data/environments/dev"
   }
 }
